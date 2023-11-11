db1026f14ef283ddfe5f033234b922eadfb8a9b2
mlnyx <mlnyx104@gmail.com>
2023-11-11 23:55:24 +0900

    Resolve merge conflict in todoDB.sqlite

--- dogsc/src/Guide.java
+++ dogsc/src/Guide.java
@@ -82,7 +82,7 @@ public class Guide extends JFrame{
         }
     }
 
-    public static void main(String[] args) {
+    public static void main() {
         SwingUtilities.invokeLater(() -> {
             new Guide();
         });
