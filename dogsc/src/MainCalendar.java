52322f6ff3b59d8e023f5cb1727422f4091d20ab
ujeong03 <yhim0320@gmail.com>
2023-11-14 12:00:18 +0900

    2주 캘린더 중간, 투두, 강아지

--- dogsc/src/MainCalendar.java
+++ dogsc/src/MainCalendar.java
@@ -10,16 +10,33 @@ import java.util.List;
 
 public class MainCalendar extends JPanel {
     private Date currentDate;
+    private Date currentStartDate;
     private JButton prevButton;
     private JButton nextButton;
+    private JButton openCalendarButton;
     private JPanel calendarPanel;
     private CalendarDBConnection calendarDB;
+
 
     public MainCalendar() {
         calendarDB = new CalendarDBConnection();
         setLayout(new BorderLayout());
 
-        currentDate = new Date(); // 오늘 날짜로 초기화
+        // 캘린더 버튼생성
+        JButton openCalendarButton = new JButton("CALENDAR");
+        // 캘린더 버튼 작동
+        openCalendarButton.addActionListener((new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new CalendarWindow();
+            }
+        }));
+
+
+        // Initialize with the current week's start and end dates
+        Calendar calendar = Calendar.getInstance();
+        currentStartDate = getStartOfWeek(calendar.getTime());
+        currentDate = currentStartDate; // Initialize currentDate
 
         // 이전 주 버튼
         prevButton = new JButton("<");
@@ -41,11 +58,12 @@ public class MainCalendar extends JPanel {
 
         // 캘린더 패널
         calendarPanel = new JPanel();
-        calendarPanel.setLayout(new GridLayout(0, 7)); // 7일씩 나열
+        calendarPanel.setLayout(new GridLayout(2, 7)); // 7일씩 나열
 
         updateCalendar(); // 캘린더 업데이트
 
         // 버튼과 캘린더 패널을 프레임에 추가
+        add(openCalendarButton,BorderLayout.NORTH);
         add(prevButton, BorderLayout.WEST);
         add(nextButton, BorderLayout.EAST);
         add(calendarPanel, BorderLayout.CENTER);
@@ -69,12 +87,27 @@ public class MainCalendar extends JPanel {
         // 현재 주와 다음 주의 일자를 가져와서 표시
         List<Date> weekDates = getWeekDates(currentDate);
         for (Date date : weekDates) {
+            final Date currentDate = date;
             JButton dateButton = new JButton(new SimpleDateFormat("MM-dd").format(date));
             dateButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     // 해당 날짜의 일정 불러오기
-                    showScheduleForDate(date);
+                    showScheduleForDate(currentDate);
+                }
+            });
+            calendarPanel.add(dateButton);
+        }
+
+        // Display dates for the next week
+        List<Date> nextWeekDates = getWeekDates(getNextWeekStart(currentStartDate));
+        for (Date date : nextWeekDates) {
+            final Date currentDate = date;
+            JButton dateButton = new JButton(new SimpleDateFormat("MM-dd").format(date));
+            dateButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    showScheduleForDate(currentDate);
                 }
             });
             calendarPanel.add(dateButton);
@@ -128,6 +161,13 @@ public class MainCalendar extends JPanel {
         calendar.setTime(startDate);
         calendar.add(Calendar.DAY_OF_MONTH, 7); // 7일 후로 이동
         calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY); // 주의 시작일로 설정
+        return calendar.getTime();
+    }
+
+    private Date getStartOfWeek(Date startDate) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(startDate);
+        calendar.add(Calendar.DAY_OF_WEEK, -6); // Move to the end of the week
         return calendar.getTime();
     }
 }
