a33b988e9586f965ec4866940d5104d15cc7241a
mlnyx <134464567+mlnyx@users.noreply.github.com>
2023-12-05 03:33:37 +0900

    Update CalendarWindow.java
    진짜  최종

--- dogsc/src/CalendarWindow.java
+++ dogsc/src/CalendarWindow.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.io.InputStream;
-import java.time.LocalDate;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -12,19 +13,12 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.CancellationException;
-
 /**
- * CalendarWindow 클래스는 캘린더 애플리케이션의 메인 윈도우를 나타냅니다.
+ * CalendarWindow 클래스는 캘린더 창을 나타내는 Swing 프레임입니다.
  */
 public class CalendarWindow extends JFrame {
-    /**
-     * 사용자 정의 폰트를 로드하기 위한 InputStream 객체입니다.
-     */
-    InputStream inputStream = getClass().getResourceAsStream("font/BMJUA_ttf.ttf");
 
-    /**
-     * 캘린더 애플리케이션에서 사용할 폰트 객체입니다.
-     */
+    InputStream inputStream = getClass().getResourceAsStream("font/BMJUA_ttf.ttf");
     Font reminderfont;
 
     {
@@ -36,64 +30,15 @@ public class CalendarWindow extends JFrame {
             throw new RuntimeException(e);
         }
     }
-    /**
-     * 선택된 날짜를 저장하는 변수입니다.
-     */
-    private Calendar selectedCalendar;
 
-    /**
-     * 캘린더 객체입니다.
-     */
-    private Calendar calendar;
+    private Calendar calendar; // 캘린더 객체
+    private JLabel monthLabel; // 월 표시 레이블
+    private JPanel calendarPanel; // 캘린더 패널
+    private JButton prevButton, nextButton; // 이전 월, 다음 월 이동 버튼
+    private final String[] dayNames = {"일", "월", "화", "수", "목", "금", "토"}; // 요일 이름 배열
 
-    /**
-     * 날짜 정보를 저장하는 변수입니다.
-     */
-    private Date currentDate;
+    private CalendarDBConnection calendarDB; // 데이터베이스 연결 객체
 
-    /**
-     * 월을 표시하는 레이블입니다.
-     */
-    private JLabel monthLabel;
-
-    /**
-     * 캘린더 패널입니다.
-     */
-    private JPanel calendarPanel;
-
-    /**
-     * 이전 월로 이동하는 버튼입니다.
-     */
-    private JButton prevButton;
-
-    /**
-     * 다음 월로 이동하는 버튼입니다.
-     */
-    private JButton nextButton;
-
-    /**
-     * 요일 이름 배열입니다.
-     */
-    private final String[] dayNames = {"일", "월", "화", "수", "목", "금", "토"};
-
-    /**
-     * 데이터베이스 연결을 관리하는 객체입니다.
-     */
-    private CalendarDBConnection calendarDB;
-
-    /**
-     * JScrollPane 목록을 저장하는 리스트입니다.
-     */
-    private List<JScrollPane> dayScrollPaneList;
-
-    /**
-     * 각 일자의 일정을 표시하기 위한 JTextArea 배열입니다.
-     */
-    private JTextArea[][] dayTextAreaArray;
-
-    /**
-     * CalendarWindow 클래스의 생성자입니다.
-     */
 
     public CalendarWindow() {
         calendar = Calendar.getInstance(Locale.getDefault()); // 로케일에 따른 현재 날짜와 시간을 가지는 캘린더 객체 생성
@@ -111,20 +56,27 @@ public class CalendarWindow extends JFrame {
 
         // 이전 월 버튼 설정
         prevButton = new JButton("<");
-        prevButton.addActionListener(e -> navigateMonths(-1));
+        prevButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calendar.add(Calendar.MONTH, -1); // 캘린더의 월을 하나 감소시킵니다.
+                updateCalendar(); // 캘린더를 업데이트합니다.
+            }
+        });
 
         // 다음 월 버튼 설정
         nextButton = new JButton(">");
-        nextButton.addActionListener(e -> navigateMonths(1));
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calendar.add(Calendar.MONTH, 1); // 캘린더의 월을 하나 증가시킵니다.
+                updateCalendar(); // 캘린더를 업데이트합니다.
+            }
+        });
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월"); // 날짜 형식 지정
-        calendar = Calendar.getInstance();
-        String month = sdf.format(calendar.getTime());
-        System.out.println("tadsf"+sdf.format(calendar.getTime()));
-        System.out.println(month);
-
-        monthLabel = new JLabel(month, JLabel.CENTER);
-        monthLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        // 월 표시 레이블 설정
+        monthLabel = new JLabel("", JLabel.CENTER);
+        monthLabel.setFont(reminderfont);
 
         monthPanel.add(Box.createHorizontalGlue());
         monthPanel.add(prevButton);
@@ -144,7 +96,7 @@ public class CalendarWindow extends JFrame {
         JPanel headerPanel = new JPanel(new GridLayout(1, 7, 5, 5)); // 그리드 레이아웃을 사용해 1행 7열로 설정
         for (String dayName : dayNames) {
             JLabel dayLabel = new JLabel(dayName, JLabel.CENTER); // 중앙 정렬된 요일 레이블
-            dayLabel.setFont(new Font("Arial", Font.BOLD, 16));
+            dayLabel.setFont(reminderfont);
             dayLabel.setForeground(Color.BLACK); // 글자색을 검은색으로 설정
             headerPanel.add(dayLabel); // 요일 레이블을 요일 헤더 패널에 추가
         }
@@ -156,67 +108,50 @@ public class CalendarWindow extends JFrame {
         setVisible(true); // 윈도우 표시
     }
 
-    /**
-     * 월을 이동하는 메서드입니다.
-     *
-     * @param delta 월 이동량 (양수는 다음 월, 음수는 이전 월)
-     */
-    private void navigateMonths(int delta) {
-        calendar.add(Calendar.MONTH, delta); // 현재 월에 delta를 더해 다음 또는 이전 월로 이동
-        updateCalendar(); // 캘린더 업데이트
-    }
 
-    /**
-     * 캘린더를 업데이트하는 메서드입니다.
-     */
+    // 캘린더 업데이트 메서드
+    // 캘린더 업데이트 메서드
     protected void updateCalendar() {
         calendarPanel.removeAll();
         JPanel daysPanel = new JPanel(new GridLayout(0, 7, 5, 5)); // 그리드 레이아웃을 사용해 일자를 표시할 패널 생성
         daysPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5)); // 테두리와 여백 설정
         calendarPanel.add(daysPanel, BorderLayout.CENTER); // 일자 패널을 캘린더 패널의 가운데에 추가
 
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월"); // 날짜 형식 지정
+        monthLabel.setText(sdf.format(calendar.getTime())); // 월 레이블에 현재 월 표시
 
-
-        calendar.set(Calendar.DAY_OF_MONTH, 1); // 현재 월의 첫 날로 설정
-        int startDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK); // 현재 월의 첫 날의 요일
-        int maxDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH); // 현재 월의 최대 일자
-
-        daysPanel.removeAll(); // 일자 패널 초기화
+        Calendar tempCalendar = (Calendar)calendar.clone();
+        tempCalendar.set(Calendar.DAY_OF_MONTH, 1); // 현재 월의 첫 날로 설정
+        int startDayOfWeek = tempCalendar.get(Calendar.DAY_OF_WEEK); // 현재 월의 첫 날의 요일
+        int maxDay = tempCalendar.getActualMaximum(Calendar.DAY_OF_MONTH); // 현재 월의 최대 일자
 
         for (int i = 1; i < startDayOfWeek; i++) {
             daysPanel.add(new JLabel("")); // 시작 요일 전까지 공백 레이블 추가
         }
 
         for (int day = 1; day <= maxDay; day++) {
-            // 여기에서 날짜별로 일정을 표시할 패널과 스크롤 패널을 생성하고 설정
             JPanel datePanel = new JPanel(new BorderLayout());
-
             JLabel dateLabel = new JLabel(String.valueOf(day), JLabel.LEFT);
-
             datePanel.add(dateLabel, BorderLayout.NORTH);
 
             // 일정을 추가하는 부분
-            List<String> schedules = calendarDB.getSchedulesForDate(calendar.getTime());
+            tempCalendar.set(Calendar.DAY_OF_MONTH, day);
+            List<String> schedules = calendarDB.getSchedulesForDate(tempCalendar.getTime());
             JPanel schedulesPanel = new JPanel(new GridLayout(schedules.size(), 1)); // 일정을 세로로 표시하기 위한 패널
 
             for (String schedule : schedules) {
-                JLabel scheduleLabel = new JLabel("V " + schedule);
+                JLabel scheduleLabel = new JLabel(" " + schedule);
                 schedulesPanel.add(scheduleLabel);
             }
 
-            JScrollPane scrollPane = new JScrollPane(schedulesPanel); // 스크롤 가능하도록 스크롤 패널에 추가
-            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED); // 수직 스크롤바 필요시 활성화
-            scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED); // 수평 스크롤바 필요시 활성화
+            JScrollPane scrollPane = new JScrollPane(schedulesPanel);
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+            scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
             datePanel.add(scrollPane, BorderLayout.CENTER);
 
-            // 스크롤바를 숨김
-            scrollPane.getVerticalScrollBar().setPreferredSize(new Dimension(0, 0));
-            scrollPane.getHorizontalScrollBar().setPreferredSize(new Dimension(0, 0));
-            scrollPane.getVerticalScrollBar().setVisible(false);
-            scrollPane.getHorizontalScrollBar().setVisible(false);
 
             final int selectedDay = day; // 선택된 일자를 저장
-            System.out.println(selectedDay+"skfWk");
+
             // 스크롤 패널에 마우스 클릭 이벤트를 추가
             scrollPane.addMouseListener(new MouseAdapter() {
                 @Override
@@ -226,19 +161,16 @@ public class CalendarWindow extends JFrame {
             });
 
             daysPanel.add(datePanel);
-            calendar.add(Calendar.DAY_OF_MONTH, 1); // 다음 날로 이동
         }
 
-        calendarPanel.revalidate(); // 일자 패널을 다시 그리기
-        calendarPanel.repaint(); // 일자 패널을 다시 그리기
+
+        calendarPanel.revalidate();
+        calendarPanel.repaint();
     }
 
 
-    /**
-     * 선택된 일자에 대한 상세 정보를 표시하는 다이얼로그를 엽니다.
-     *
-     * @param selectedDay 선택된 일자
-     */
+
+    // DateDetailDialog를 여는 메서드
     private void openDateDetailDialog(int selectedDay) {
         // 현재 선택된 월과 선택된 일자를 사용하여 날짜 생성
         calendar = Calendar.getInstance();
@@ -248,7 +180,7 @@ public class CalendarWindow extends JFrame {
 
         // 선택된 날짜 가져오기
         Date selectedDate = new Date(selectedCalendar.getTimeInMillis());
-        System.out.println(selectedDate+"선택날짜");
+
 
 
         // DateDetailDialog 열기
@@ -256,15 +188,7 @@ public class CalendarWindow extends JFrame {
         detailDialog.setVisible(true);
     }
 
-
-
-    /**
-     * 캘린더 애플리케이션의 진입점입니다.
-     *
-     * @param args 명령줄 인수 (사용하지 않음)
-     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new CalendarWindow()); // Swing 스레드에서 윈도우 생성
     }
 }
-
