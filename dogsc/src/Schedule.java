b0a727852fa6266a329348c8fcc305f9025227f2
mlnyx <134464567+mlnyx@users.noreply.github.com>
2023-12-04 23:49:56 +0900

    Update Schedule.java
    주석

--- dogsc/src/Schedule.java
+++ dogsc/src/Schedule.java
@@ -1,12 +1,22 @@
 import java.util.Date;
-
+/**
+ * Schedule 클래스는 캘린더 일정을 나타내는 데이터 모델입니다.
+ */
 public class Schedule {
     private int id;
     private String text;
     private Date date;
     private boolean isReminder;
     private boolean isHomework;
-
+    /**
+     * Schedule 클래스의 생성자입니다.
+     *
+     * @param id        일정 고유 식별자
+     * @param text      일정 내용
+     * @param date      일정 날짜
+     * @param isReminder 리마인더 여부
+     * @param isHomework 과제 여부
+     */
     public Schedule(int id, String text, Date date, boolean isReminder, boolean isHomework) {
         this.id = id;
         this.text = text;
@@ -15,7 +25,11 @@ public class Schedule {
         this.isHomework = isHomework;
     }
 
-    // Getters and Setters
+    /**
+     * 일정 고유 식별자를 반환합니다.
+     *
+     * @return 일정 고유 식별자
+     */
     public int getId() {
         return id;
     }
@@ -23,7 +37,11 @@ public class Schedule {
     public void setId(int id) {
         this.id = id;
     }
-
+    /**
+     * 일정 내용을 반환합니다.
+     *
+     * @return 일정 내용
+     */
     public String getText() {
         return text;
     }
@@ -39,7 +57,11 @@ public class Schedule {
     public void setDate(Date date) {
         this.date = date;
     }
-
+    /**
+     * 리마인더 여부를 설정합니다.
+     *
+     * @return 리마인더 여부
+     */
     public boolean isReminder() {
         return isReminder;
     }
@@ -47,7 +69,11 @@ public class Schedule {
     public void setReminder(boolean reminder) {
         isReminder = reminder;
     }
-
+    /**
+     * 과제 여부를 반환합니다.
+     *
+     * @return 과제 여부
+     */
     public boolean isHomework() {
         return isHomework;
     }
